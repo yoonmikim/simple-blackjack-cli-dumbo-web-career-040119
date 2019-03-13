3d4924f7b0cb8d040a97eb79ea725e01ff21efaf
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-13 05:07:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,7 +67,7 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  until card_total >= 21 do
+  until card_total >= 21
   card_total = hit?(card_total)
   display_card_total(card_total)
   end
