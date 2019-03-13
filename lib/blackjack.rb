e40c01509095925e1dc3f2a0e1225cb3eb0eb352
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-13 05:08:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,7 +57,7 @@ end
 
 def invalid_command
   # code invalid_command here
-     puts "Please enter a valid command"l
+     puts "Please enter a valid command"
 end
 
 #####################################################
