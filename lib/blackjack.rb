31cf54d373b81cac99973c0ed84639a7f5a10ca8
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-13 05:04:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,7 +68,8 @@ def runner
   card_total = initial_round
   until card_total >= 21 do
   card_total = hit?(card_total)
-  display_card_total
+  display_card_total(card_total)
   end
+end_game
 end
     
